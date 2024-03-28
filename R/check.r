8a1b99b26316df251c0ab1f16e1e348f9e8658dd
James Goldie <james.goldie@360info.org>
2024-03-28 11:34:47 +1100

    Improve etag logic

--- R/check.r
+++ R/check.r
@@ -9,7 +9,18 @@ country_url <- paste0(
   "https://github.com/wmgeolab/geoBoundaries/",
   "raw/main/releaseData/CGAZ/geoBoundariesCGAZ_ADM0.gpkg")
 
-if (!file.exists("etag")) {
+# request HEAD
+request(country_url) |>
+  req_method("HEAD") |>
+  req_perform() ->
+head_req
+
+# get the new etag
+head_req |>
+  resp_header("etag") ->
+new_etag
+
+if (!file.exists("data/etag")) {
 
   # no existing etag: need new boundaries
   message("No existing etag found; update needed")
@@ -17,19 +28,8 @@ if (!file.exists("etag")) {
 
 } else {
 
-  # request HEAD
-  request(country_url) |>
-    req_method("HEAD") |>
-    req_perform() ->
-  head_req
-
-  # get the etag
-  head_req |>
-    resp_header("etag") ->
-  new_etag
-
   # get the existing etag from disk
-  current_etag <- readLines("etag")
+  current_etag <- readLines("data/etag")
   
   # report result to gha
   is_stale <- tolower(as.character(current_etag != new_etag))
@@ -37,8 +37,9 @@ if (!file.exists("etag")) {
 
   write_to_gha_env("IS_STALE", is_stale)
 
-  if (is_stale) {
-    writeLines(new_etag, "etag")
-  }
+}
 
+if (!file.exists("data/etag") || is_stale) {
+  message("Writing out new etag")
+  writeLines(new_etag, "data/etag")
 }
